efadd913e44309806847cc9bfe093483c7df79cb
Guillermo <gposebon@gmail.com>
2021-09-17 22:37:20 -0300

    ultimos cambios

--- src/com/global/GlobalVariablesData.java
+++ src/com/global/GlobalVariablesData.java
@@ -2,6 +2,7 @@ package com.global;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -9,11 +10,14 @@ public class GlobalVariablesData {
 
 	public static void main(String[] args) throws IOException {
 		Properties prop = new Properties();
-		FileInputStream fis = new FileInputStream("C:\\Git\\Selenium_Automation\\src\\data.properties");
+		FileInputStream fis = new FileInputStream("C:\\Users\\Usuario\\Documents\\Git\\Selenium_Automation\\src\\data.properties");
 		prop.load(fis);
 		System.out.println(prop.getProperty("browser"));
 		System.out.println(prop.getProperty("url"));
-
+		prop.setProperty("browser", "Mozilla");
+		System.out.println(prop.getProperty("browser"));
+		FileOutputStream fos = new FileOutputStream("C:\\Users\\Usuario\\Documents\\Git\\Selenium_Automation\\src\\data.properties");
+		prop.store(fos, null);
 	}
 
 }
